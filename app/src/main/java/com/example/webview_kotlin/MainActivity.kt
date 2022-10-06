9881e89dd1b1eb29ff6cba332e1150661892e3d8
Aman <eyeon4840@gmail.com>
2022-10-06 16:26:33 +0530

    load basic WebView in KOTLIN

--- app/src/main/java/com/example/webview_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/webview_kotlin/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.webview_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.webview_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding :ActivityMainBinding
+    private val URL = "https://developer.android.com/topic/libraries/view-binding"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        binding.webView.loadUrl(URL)
 
 
     }
