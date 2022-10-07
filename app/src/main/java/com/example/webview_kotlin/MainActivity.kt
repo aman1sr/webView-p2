303d7cda5eab2a8fd86f79b87a5b09da43852346
Aman <eyeon4840@gmail.com>
2022-10-07 23:04:05 +0530

    progress bar added

--- app/src/main/java/com/example/webview_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/webview_kotlin/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.webview_kotlin
 
+import android.app.ProgressDialog
+import android.content.Context
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.example.webview_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +24,49 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
-        binding.webView.loadUrl(URL)
+        binding.webView.apply {
+            this.settings.loadsImagesAutomatically = true
+            this.settings.javaScriptEnabled = true
+            binding.webView.scrollBarSize = View.SCROLLBARS_INSIDE_OVERLAY
+            binding.webView.webViewClient = WebViewClient()
+
+            binding.webView.loadUrl(URL)
+        }
+
+        val progressDialog: ProgressDialog? = ProgressDialog(this)
+
+        binding.webView.webViewClient = object: WebViewClient() {
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                progressDialog?.setTitle("Loading...")
+                progressDialog?.setMessage("Please wait...")
+                progressDialog?.setCancelable(false)
+                progressDialog?.show()
+
+            }
+
+            override fun onPageCommitVisible(view: WebView?, url: String?) {
+                super.onPageCommitVisible(view, url)
+
+                progressDialog?.dismiss()
+
+                /*
+                * Ab. statement is same as :
+                *                            if (progressDialog != null) {
+                                                 progressDialog.dismiss()
+                                                  }
+                *
+                * */
+            }
+        }
+
+
+
+
+
+
+
 
 
     }
